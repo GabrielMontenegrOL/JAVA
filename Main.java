54567709d047b28a82d72818491f2d1947cf17c6
Gabriel Montenegro <131418339+GabrielMontenegrOL@users.noreply.github.com>
2024-02-20 10:12:37 -0300

    aula

---
+++ Main.java
@@ -0,0 +1,17 @@
+package aula;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Universe");
+		go();
+		System.out.println("Hello world");
+		String nome = "Gabriel";
+		System.out.println(nome + " Chegamos ao final da aula");
+	}
+	
+	public static void go () {
+		System.out.println("To indo...");
+	}
+
+}
